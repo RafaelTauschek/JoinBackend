f6abb8f2c897fef8b221ac6c9aba280be5b9475d
Rafael Tauschek <rafael.tauschek@gmail.com>
2024-03-11 21:43:38 +0100

    added url

--- join/urls.py
+++ join/urls.py
@@ -1,21 +1,18 @@
-"""join URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from join_backend.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view()),
+    path('register/', RegisterView.as_view()),
+    path('tasks/', TaskView.as_view()),
+    path('tasks/<int:pk>', TaskView.as_view()),
+    path('contacts/', ContactView.as_view()),
+    path('contacts/<int:pk>', ContactView.as_view()),
+    path('subtasks/', SubtaskView.as_view()),
+    path('subtasks/<int:pk>', SubtaskView.as_view()),
+    path('users/', UserView.as_view()),
+    path('users/<int:pk>', UserView.as_view()),
 ]
+
