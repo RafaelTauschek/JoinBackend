6c45d2df2b4f3718285fe8672186be4a245789ef
Rafael Tauschek <rafael.tauschek@gmail.com>
2024-03-25 20:28:04 +0100

    subtask test

--- join_backend/serializers.py
+++ join_backend/serializers.py
@@ -1,29 +1,26 @@
 from rest_framework import serializers
 from .models import *
 from django.contrib.auth.models import User
-
-class TaskSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
-    class Meta:
-        model = Task
-        # fields = ['id', 'assigned_to', 'description', 'due_date', 'prio', 'status', 'title']
-        fields = '__all__'
-        
-        
+   
         
 class CategorySerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
     class Meta:
         model = Category
         fields = '__all__'
-        
-        
+             
 class SubtaskSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
     class Meta:
         model = Subtask
         fields = '__all__'
-        
+          
+class TaskSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+    subtasks = SubtaskSerializer(many=True, read_only=True)
+    class Meta:
+        model = Task
+        fields = '__all__'       
 
 class ContactSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
